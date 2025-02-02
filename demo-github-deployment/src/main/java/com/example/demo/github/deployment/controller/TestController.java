7c9570fa27bec7333b1ecb72dd23387e3ce9ad76
Nagarro\akshataggarwal <aaakshataggarwal5@gmail.com>
2025-02-02 17:20:41 +0530

    First Commit

---
+++ demo-github-deployment/src/main/java/com/example/demo/github/deployment/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.github.deployment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping
+	public String greet() {
+		return "Hello World";
+	}
+}
